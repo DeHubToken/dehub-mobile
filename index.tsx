28d3cbdb9384fc9a88630fde2ad8787a7f5b4396
Alhaji <alhaji4megahit@gmail.com>
2025-07-27 01:31:29 +0100

    feat: did a bunch of local configs

--- index.tsx
+++ index.tsx
@@ -1,4 +1,10 @@
+import 'react-native-gesture-handler';
 import { registerRootComponent } from 'expo';
 import App from './App';
+
+// Polyfill for _toString if needed
+// if (typeof global !== 'undefined' && !global._toString) {
+//   global._toString = Object.prototype.toString;
+// }
 
 registerRootComponent(App);
