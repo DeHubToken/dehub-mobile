50da9611979d52e5a17847ae78635ea6c6eebe6f
maldoteth <dev@dehub.io>
2026-09-11 02:46:46 +0100

    Match home badges to name size and leaderboard spacing

--- components/Home/FeedCardHeader.tsx
+++ components/Home/FeedCardHeader.tsx
@@ -44,7 +44,7 @@ const FeedCardHeaderComponent: React.FC<FeedCardHeaderProps> = ({
       </Pressable>
 
       <View className="flex-1 min-w-0 mr-2">
-        <View style={{ flexDirection: "row", alignItems: "center", minWidth: 0 }}>
+        <View style={{ flexDirection: "row", alignItems: "baseline", minWidth: 0 }}>
           <Text
             className="font-semibold"
             style={{ color: "#F9FBFF", flexShrink: 1, fontSize: 16, lineHeight: 20 }}
@@ -57,10 +57,10 @@ const FeedCardHeaderComponent: React.FC<FeedCardHeaderProps> = ({
           {badgeImage && (
             <Pressable
               onPress={onUserPress}
-              style={{ flexShrink: 0, marginLeft: 4 }}
+              style={{ flexShrink: 0, alignSelf: "center" }}
               hitSlop={{ top: 6, bottom: 6, left: 6, right: 6 }}
             >
-              <Image source={badgeImage} style={getBadgeOpticalStyle(badgeImage, 14, -1)} resizeMode="contain" />
+              <Image source={badgeImage} style={getBadgeOpticalStyle(badgeImage, 16)} resizeMode="contain" />
             </Pressable>
           )}
           {address && (
